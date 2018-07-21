560eba5f6fb9d52c2792cf4caab4be81861b4510
rameshkumar <rameshkumar@ajaxmediatech.com>
2018-07-21 17:31:59 +0530

    Product completed  Commit

--- models/leave_management/leave_voucher.py
+++ models/leave_management/leave_voucher.py
@@ -18,12 +18,12 @@ class LeaveVoucher(surya.Sarpam):
     person_id = fields.Many2one(comodel_name="hos.person", string="Person")
     count = fields.Float(string="Count")
     difference = fields.Float(string="Difference")
-    debit_lines = fields.One2many(comodel_name="leave.voucher.line",
-                                  inverse_name="debit_id",
+    debit_lines = fields.One2many(comodel_name="leave.voucher.debit.line",
+                                  inverse_name="voucher_id",
                                   string="Leave Voucher Line")
 
-    credit_lines = fields.One2many(comodel_name="leave.voucher.line",
-                                   inverse_name="credit_id",
+    credit_lines = fields.One2many(comodel_name="leave.voucher.credit.line",
+                                   inverse_name="voucher_id",
                                    string="Leave Voucher Line")
 
     is_manual = fields.Boolean(string="Is Manual")
@@ -33,57 +33,43 @@ class LeaveVoucher(surya.Sarpam):
 
     @api.onchange("count")
     def update_count(self):
-        # Reset all reconcile
-        for rec in self.debit_lines:
-            rec.leave_reconcile = 0
+        cr_line = self.credit_lines
+        dr_line = self.debit_lines
 
-        for rec in self.credit_lines:
-            rec.leave_reconcile = 0
+        count = self.count
 
-        # Credit Reconcilation
-        leave_days = self.count
-
-        for rec in self.debit_lines:
-            rec.reconcile = True
-            leave_days = leave_days + rec.debit
-
-        for rec in self.credit_lines:
-            value = rec.credit - rec.leave_reconcile
-            total = value - leave_days
-
-            if (total >= 0) and (value > 0):
-                rec.reconcile = True
-                rec.leave_reconcile = rec.leave_reconcile + leave_days
-                leave_days = 0
-
-            elif (total < 0) and (value > 0):
-                rec.reconcile = True
-                rec.leave_reconcile = rec.leave_reconcile + value
-                leave_days = leave_days - value
-
-        # Debit reconcilation
-        credit_reconciled = 0
-
-        for rec in self.credit_lines:
-            if rec.reconcile:
-                credit_reconciled = credit_reconciled + rec.leave_reconcile
-
-        debit = credit_reconciled - self.count
-
-        for rec in self.debit_lines:
+        for rec in cr_line:
             rec.reconcile = False
-            value = rec.debit - debit
+            rec.leave_reconcile = 0
 
-            if value >= 0:
-                rec.reconcile = True
-                rec.leave_reconcile = debit
-                debit = 0
-            else:
-                rec.reconcile = True
-                rec.leave_reconcile = rec.debit
-                debit = debit - rec.debit
+        for rec in dr_line:
+            rec.reconcile = False
+            rec.leave_reconcile = 0
 
-        self.difference = leave_days
+        for cr in cr_line:
+            if (cr.credit - cr.leave_reconcile) > 0:
+                if (cr.credit - cr.leave_reconcile) >= count:
+                    cr.leave_reconcile = cr.leave_reconcile + count
+                    cr.reconcile = True
+
+                elif (cr.credit - cr.leave_reconcile) < count:
+                    cr.reconcile = True
+                    cr.leave_reconcile = cr.leave_reconcile + (cr.credit - cr.leave_reconcile)
+
+        for dr in dr_line:
+            for cr in cr_line:
+                if (cr.credit - cr.leave_reconcile) > 0:
+                    if (cr.credit - cr.leave_reconcile) >= (dr.debit - dr.leave_reconcile):
+                        cr.leave_reconcile = cr.leave_reconcile + (dr.debit - dr.leave_reconcile)
+                        dr.leave_reconcile = dr.leave_reconcile + (dr.debit - dr.leave_reconcile)
+                        cr.reconcile = True
+                        dr.reconcile = True
+
+                    elif (cr.credit - cr.leave_reconcile) < (dr.debit - dr.leave_reconcile):
+                        cr.reconcile = True
+                        dr.reconcile = True
+                        dr.leave_reconcile = dr.leave_reconcile + (cr.credit - cr.leave_reconcile)
+                        cr.leave_reconcile = cr.leave_reconcile + (cr.credit - cr.leave_reconcile)
 
     @api.onchange("person_id")
     def get_cr_dr_lines(self):
@@ -93,8 +79,7 @@ class LeaveVoucher(surya.Sarpam):
 
             recs_cr = self.env["leave.item"].search([("person_id", "=", self.person_id.id),
                                                      ("credit", ">", 0),
-                                                     ("reconcile_id", "=", False),
-                                                     ("reconcile_partial_id", "=", False)])
+                                                     ("reconcile_id", "=", False)])
             for rec in recs_cr:
                 data = {}
 
@@ -113,8 +98,7 @@ class LeaveVoucher(surya.Sarpam):
             res_dr = []
             recs_dr = self.env["leave.item"].search([("person_id", "=", self.person_id.id),
                                                      ("debit", ">", 0),
-                                                     ("reconcile_id", "=", False),
-                                                     ("reconcile_partial_id", "=", False)])
+                                                     ("reconcile_id", "=", False)])
             for rec in recs_dr:
                 data = {}
 
@@ -172,21 +156,33 @@ class LeaveVoucher(surya.Sarpam):
         return vals
 
 
-class LeaveVoucherLine(surya.Sarpam):
-    _name = "leave.voucher.line"
+class LeaveVoucherCreditLine(surya.Sarpam):
+    _name = "leave.voucher.credit.line"
 
     date = fields.Date(string="Date", required=True)
     person_id = fields.Many2one(comodel_name="hos.person", string="Person")
     name = fields.Char(string="Name", required=True)
     description = fields.Text(string="Description")
-    credit_id = fields.Many2one(comodel_name="leave.voucher", string="Leave Voucher")
-    debit_id = fields.Many2one(comodel_name="leave.voucher", string="Leave Voucher")
+    voucher_id = fields.Many2one(comodel_name="leave.voucher", string="Leave Voucher")
     credit = fields.Float(string="Credit")
-    debit = fields.Float(string="Debit")
     reconcile = fields.Boolean(string="Reconcile")
     journal_id = fields.Many2one(comodel_name="leave.item", string="Journal Item")
     leave_reconcile = fields.Float(string="Leave Reconcile")
     difference = fields.Float(string="Difference")
 
 
+
+class LeaveVoucherDebitLine(surya.Sarpam):
+    _name = "leave.voucher.debit.line"
+
+    date = fields.Date(string="Date", required=True)
+    person_id = fields.Many2one(comodel_name="hos.person", string="Person")
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description")
+    voucher_id = fields.Many2one(comodel_name="leave.voucher", string="Leave Voucher")
+    debit = fields.Float(string="Debit")
+    reconcile = fields.Boolean(string="Reconcile")
+    journal_id = fields.Many2one(comodel_name="leave.item", string="Journal Item")
+    leave_reconcile = fields.Float(string="Leave Reconcile")
+    difference = fields.Float(string="Difference")
 
