867fef5d1a59514edc465a4123bbedc6a6aa0d66
rameshkumar <rameshkumar@ajaxmediatech.com>
2018-07-25 10:21:35 +0530

    Product completed  Commit

--- models/account/voucher.py
+++ models/account/voucher.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, api
+from odoo import fields, api, exceptions
 from .. import surya
 
 PAYMENT_TYPE = [("payment_in", "Payment In"), ("payment_out", "Payment Out")]
@@ -26,6 +26,91 @@ class Voucher(surya.Sarpam):
     debit_detail = fields.One2many(comodel_name="hos.voucher.debit",
                                    inverse_name="voucher_id",
                                    string="Debit Detail")
+
+    def check_reconcile(self):
+        cr_line = self.env["hos.voucher.credit"].search([("reconcile", "=", True), ("voucher_id", "=", self.id)])
+        dr_line = self.env["hos.voucher.debit"].search([("reconcile", "=", True), ("voucher_id", "=", self.id)])
+        amount = self.amount
+        difference = self.difference
+
+        total_amount = amount - difference
+        for rec in dr_line:
+            if rec.reconcile:
+                total_amount = total_amount + rec.amount_allocated
+
+        for rec in cr_line:
+            if rec.reconcile:
+                total_amount = total_amount - rec.amount_allocated
+
+        if total_amount:
+            raise exceptions.ValidationError("Error! Amount reconcile greater than given amount")
+
+    def journal_creation(self, reconcile_id, description, account_id, cr_value=0, dr_value=0):
+        data = {}
+
+        data["reference"] = self.name
+        data["person_id"] = self.person_id.id
+        data["description"] = description
+        data["credit"] = cr_value
+        data["debit"] = dr_value
+        data["reconcile_id"] = reconcile_id
+        data["account_id"] = account_id
+
+        return data
+
+    @api.multi
+    def trigger_posting(self):
+        self.check_reconcile()
+        cr_line = self.credit_detail
+        dr_line = self.debit_detail
+
+        reconcile_id = self.env["hos.reconcile"].crete()
+
+        journal_detail = []
+        for cr in cr_line:
+            if cr.amount_allocation:
+                data = self.journal_creation(reconcile_id=reconcile_id.id,
+                                             description="",
+                                             account_id="",
+                                             cr_value=cr.amount_allocation,
+                                             dr_value=0)
+
+                journal_detail.append(data)
+
+                data = self.journal_creation(reconcile_id=reconcile_id.id,
+                                             description="",
+                                             account_id="",
+                                             cr_value=cr.credit - cr.amount_allocation,
+                                             dr_value=0)
+
+                journal_detail.append(data)
+
+        for dr in dr_line:
+            if dr.amount_allocation:
+                data = self.journal_creation(reconcile_id=reconcile_id.id,
+                                             description="",
+                                             account_id="",
+                                             cr_value=0,
+                                             dr_value=dr.amount_allocation)
+
+                journal_detail.append(data)
+
+                data = self.journal_creation(reconcile_id=reconcile_id.id,
+                                             description="",
+                                             account_id="",
+                                             cr_value=dr.debit - dr.amount_allocation,
+                                             dr_value=0)
+
+                journal_detail.append(data)
+
+        if self.amount:
+            data = self.journal_creation(reconcile_id=reconcile_id.id,
+                                         description="",
+                                         account_id="",
+                                         cr_value=0,
+                                         dr_value=self.amount)
+
+            journal_detail.append(data)
 
     @api.onchange('person_id')
     def get_cr_dr_lines(self):
@@ -116,14 +201,6 @@ class Voucher(surya.Sarpam):
                         cr.amount_allocated = cr.amount_allocated + cr_diff
 
         # Get Difference
-        for rec in cr_line:
-            if rec.reconcile:
-                amount = amount - rec.credit
-
-        for rec in dr_line:
-            if rec.reconcile:
-                amount = amount + rec.debit
-
         self.difference = amount
 
 
@@ -140,6 +217,11 @@ class VoucherCreditLine(surya.Sarpam):
     voucher_id = fields.Many2one(comodel_name="hos.voucher", string="Voucher")
     reconcile = fields.Boolean(string="Reconcile")
     item_id = fields.Many2one(comodel_name="journal.item", string="Journal Item")
+    difference = fields.Float(string="Difference", store=False, compute="_get_difference")
+
+    def _get_difference(self):
+        for rec in self:
+            rec.difference = rec.credit - rec.amount_allocated
 
 
 class VoucherDebitLine(surya.Sarpam):
@@ -155,3 +237,7 @@ class VoucherDebitLine(surya.Sarpam):
     voucher_id = fields.Many2one(comodel_name="hos.voucher", string="Voucher")
     reconcile = fields.Boolean(string="Reconcile")
     item_id = fields.Many2one(comodel_name="journal.item", string="Journal Item")
+
+    def _get_difference(self):
+        for rec in self:
+            rec.difference = rec.debit - rec.amount_allocated
